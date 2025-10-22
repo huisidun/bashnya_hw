7253624dac09500c0d64fbd928de8b4c6f5f0664
Angelina Gridasova <gridasovaangelina@mail.ru>
2025-10-22 12:36:14 +0300

    some fix

--- bashnya-hw2/hw2.go
+++ bashnya-hw2/hw2.go
@@ -6,32 +6,37 @@ func main() {
 
 	var num int
 
-	fmt.Println("введите целое число меньшее 12307")
-	fmt.Scanf("%d", &num)
+	for {
+		
+		fmt.Println("введите целое число меньшее 12307")
+		fmt.Scanf("%d", &num)
 
-	if num > 12307 {
-		fmt.Println("число должно быть меньше 12307")
-	} else {
-		for (num < 12307) {
-			if num < 0 {
-				num *= -1
-			} else if num%7==0 {
-				num *= 39
-			} else if num%9==0 {
-				num *= 13
-				num += 1
-				continue
-			} else {
-				num += 2 
-				num *= 3
+		if num > 12307 {
+			fmt.Println("число должно быть меньше 12307!")
+			continue
+		} else {
+			for (num < 12307) {
+				if num < 0 {
+					num *= -1
+				} else if num%7==0 {
+					num *= 39
+				} else if num%9==0 {
+					num *= 13
+					num += 1
+					continue
+				} else {
+					num += 2 
+					num *= 3
+				}
+				if num%13==0 &&num%9==0 {
+					fmt.Println("service error")
+					break
+				} else {
+					num +=1
+				}
 			}
-			if num%13==0 &&num%9==0 {
-				fmt.Println("service error")
-				break
-			} else {
-				num +=1
-			}
+			fmt.Printf("новое значение: %v, тип переменной: %T", num, num)
+			break
 		}
-		fmt.Printf("новое значение: %v, тип переменной: %T", num, num)
 	}
 }
